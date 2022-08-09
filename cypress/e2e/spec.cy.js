bde48a942756243cea4a30eafd991e6d5da0df90
“Corinne <corinne.canavan@gmail.com>
2022-08-09 11:09:39 -0600

    Add test for form inputs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,7 @@ describe('URL Shortener User Flows', () => {
     })
     cy.visit('http://localhost:3000/')
   })
+
   it('should be able to visit page and render title and existing URLs', () => {
     cy.get('header').contains('h1', 'URL Shortener')
     cy.get('section').find('.url').should('have.length', 1)
@@ -13,5 +14,11 @@ describe('URL Shortener User Flows', () => {
     cy.get('.url').contains('h3', 'Awesome photo')
     cy.get('a').should('contain', 'http://localhost:3001/useshorturl/1')
     cy.get('p').should('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  })
+
+  it('should be able to render form with proper inputs', () => {
+    cy.get('form').find('input').should('have.length', 2)
+    cy.get('input[name="title"]').should('be.visible')
+    cy.get('input[name="urlToShorten"]').should('be.visible')
   })
 })
--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -3,7 +3,6 @@ import './UrlContainer.css';
 
 const UrlContainer = props => {
   const urlEls = props.urls.map(url => {
-    console.log('props', props)
     return (
       <div className="url" key={url.id} id={url.id}>
         <h3>{url.title}</h3>
